1f3d15428447fa924476581ac248cf588496ec88
KiritoKarb <110470105+KiritoKarb@users.noreply.github.com>
2022-10-13 18:50:45 +0200

    Ready

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from people import *
 from salary import calculate_salary
+from  datetime import datetime
 
 if __name__ == '__main__':
     maan = Person("1234")
     maan.salary = [1, 4, 5, 6, 2, 76, 11]
 
-    calculate_salary(man)
-    calculate_salary(maan)
+    print(f'{datetime.now()} salary {calculate_salary(man)}')
+    print(f'{datetime.now()} salary {calculate_salary(maan)}')
--- salary.py
+++ salary.py
@@ -2,4 +2,4 @@ def calculate_salary(person):
     x = 0
     for i in person.salary:
         x += i
-    return print(x)
+    return x
